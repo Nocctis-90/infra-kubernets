51125f0f02dfb0d0ca1fd9dc2dfbd6f599b67b15
Nocctis-90 <munhoz.raul90@gmail.com>
2024-03-18 21:24:44 -0300

    initial commit

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "cluster_name" {
+  description = "Name of the Kubernetes cluster"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
